9c8cc3da091070c63e26b35ad7a58b649981b768
cwp10 <cwp10@msn.com>
2015-11-11 22:21:18 +0900

    Day2

---
+++ Assets/Scripts/LegacySample1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LegacySample1 : MonoBehaviour {
+
+    public Animation animation;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        animation = GetComponent<Animation>();
+        AnimationState idleState = animation["idle"];
+        idleState.wrapMode = WrapMode.Loop;
+        idleState.normalizedTime = 0.0f;
+        idleState.speed = 1.0f;
+
+        animation.Play("idle");
+
+        yield return new WaitForSeconds(0.4f);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            animation.Play("attack_1");
+            animation.PlayQueued("idle", QueueMode.CompleteOthers);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            animation.Play("attack_2");
+            animation.PlayQueued("idle", QueueMode.CompleteOthers);
+        }
+    }
+
+    void OnAttack1() {
+        AnimationState attackState = GetComponent<Animation>()["attack_1"];
+        Debug.Log("attack_1" + attackState.length.ToString());
+        Debug.Log("attack_1" + attackState.time.ToString());
+    }
+    void OnAttack2() {
+        AnimationState attackState = GetComponent<Animation>()["attack_2"];
+        Debug.Log("attack_2" + attackState.length.ToString());
+        Debug.Log("attack_2" + attackState.time.ToString());
+    }
+}
---
+++ Assets/Scripts/Warrior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warrior : MonoBehaviour {
+
+    Animator _animator;
+
+    void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            _animator.SetTrigger("attack");
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            _animator.SetTrigger("hit");
+        }
+
+    }
+}
